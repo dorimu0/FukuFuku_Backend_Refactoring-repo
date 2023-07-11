4693708c1a63e927e809d2b0d2dc80642f3f366c
Z00One <9000248@g.yju.ac.kr>
2023-07-11 22:44:40 +0900

    refact: Modify to config .env file

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,28 +1,27 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { UserInfo as User } from './entity/userInfo.entity';
 import { JwtService } from '@nestjs/jwt';
-// import { config } from 'dotenv'
-// config();
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private userService: UserService,
-    private jwtService: JwtService
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService
   ) { }
 
-  async generateAccessToken(user: User): Promise<string> {
-    const payload = {
-      email: user.email
-    }
+  async generateAccessToken(email: string): Promise<string> {
+    const payload = { email }
 
     const token = await this.jwtService.signAsync(payload, {
-      secret: process.env.JWT_REFRESH_SECRET,
-      expiresIn: process.env.JWT_REFRESH_EXPRIESIN
+      secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+      expiresIn: this.configService.get<string>('JWT_ACCESS_EXPRIESIN')
     });
-
-    return token;
+    // response.appendHeader('Authorization')
+    // response.setHeader('Authorization',`Bearer ${token}`);
+    return "Bearer " + token;
   }
 
   async generateRefreshToken(user: User): Promise<string> {
@@ -34,17 +33,17 @@ export class AuthService {
     }
 
     const token = await this.jwtService.signAsync(payload, {
-      secret: process.env.JWT_REFRESH_SECRET,
-      expiresIn: process.env.JWT_REFRESH_EXPRIESIN,
+      secret: this.configService.get<string>('JWT_REFRESH_SECRET'),
+      expiresIn: this.configService.get<string>('JWT_REFRESH_EXPRIESIN'),
     });
 
     return token;
   }
 
   // access 토큰 안되면 refresh 토큰 가져오기
-  async refresh(token: string) {
+  refresh(token: string) {
     // decoded 토큰을 이용해 유저의 정보 가져오기
-    const decodedToken = await this.jwtService.decode(token);
-    console.log(decodedToken);
+    const decodedToken = this.jwtService.decode(token);
+    return decodedToken;
   }
 }
