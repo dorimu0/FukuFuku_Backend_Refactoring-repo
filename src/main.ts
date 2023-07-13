372a9143e40f56862ea6121b22cc6109f24003fb
Z00One <9000248@g.yju.ac.kr>
2023-07-13 23:30:17 +0900

    refact: Remove unnecessary imports, code

--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 import { ValidationPipe } from '@nestjs/common';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
-
-dotenv.config({
-  path: path.resolve(
-    (process.env.NODE_ENV === 'production') ? '.production.env'
-      : (process.env.NODE_ENV === 'stage') ? '.stage.env' : '.env'
-  )
-});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // 데코레이터를 가지지 않는 프로퍼티(dto에 맞지 않는 데이터)에서 유효성 검사된 객체 제거
-    forbidNonWhitelisted: true, // whitelist의 객체를 제거하지 않고 Error throw - controller까지 도달하지 않음
-    transform: true // 요청 들어온 데이터를 type에 맞게 변경
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
   }))
+
   await app.listen(3000);
 }
 bootstrap();
