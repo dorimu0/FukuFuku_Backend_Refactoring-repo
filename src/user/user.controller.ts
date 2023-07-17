d7202dde96b895207f6ad0bf8177e391722fcb27
Z00One <9000248@g.yju.ac.kr>
2023-07-17 23:26:04 +0900

    feat: Implement user update api and delete api
    - update nickname
    - update introduction
    - update picture
    - withdraw

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,60 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Body, Param, Put, Patch, Delete, Req, UseInterceptors, Headers } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/create-user.dto'
-
+import { UpdateUserIntroductionDto, UpdateUserNicknameDto, UpdateCommonWhere as UpdateUserDto } from './dto/update-user.dto';
+import { IsAuthenticable } from 'src/common/decorators/authentic.decorator';
+import { responseFormat, OK, NoContent } from '../common/util/responseFormat'
+import { UserDeleteWhereDto } from './dto/delete-user.dto';
+import { S3Interceptor } from '../common/util/upload.interceptor';
+import { S3Client } from '@aws-sdk/client-s3';
 @Controller('user')
 export class UserController {
   constructor(
-    private readonly userService: UserService
+    private readonly userService: UserService,
   ) { }
 
-  // @Post()
-  // async signIn(@Body() createUserDto: CreateUserDto) {
-  //   const userInfo = await this.userService.signIn(createUserDto);
-  //   return userInfo;
-  // }
+  // 닉네임 중복 체크
+  // @UseGuards(AccessGuard)
+  @Get('check/:nickName')
+  async isExistNickname(@Param('nickName') nickname: string) {
+    await this.userService.nicknameDuplicateCheck(nickname, 'request');
+    return responseFormat(OK);
+  }
+
+  // 닉네임 수정
+  @IsAuthenticable('author')
+  @Put('/editNickname')
+  async editNickname(@Body('data') data: UpdateUserNicknameDto) {
+    const result = await this.userService.editNickname(data);
+
+    // UX를 위한 데이터 response
+    return responseFormat(OK, result);
+  }
+
+  // 자기 소개 수정
+  @IsAuthenticable('author')
+  @Patch('/editIntroduction')
+  async editIntroduction(@Body('data') updateData: UpdateUserIntroductionDto) {
+    const result = await this.userService.editIntroduction(updateData);
+
+    return responseFormat(OK, result);
+  }
+
+  // 회원 탈퇴
+  @IsAuthenticable('author')
+  @Delete('/withdraw')
+  async withdraw(@Body('data') userData: UserDeleteWhereDto) {
+    await this.userService.withdraw(userData);
+
+    return responseFormat(NoContent);
+  }
+
+  // 이미지 수정
+  @IsAuthenticable('author')
+  @Put('editImage')
+  @UseInterceptors(S3Interceptor)
+  async editImage(@Req() req, @Headers('data') userData: UpdateUserDto) {
+    const result = await this.userService.editPicture(userData, req);
+
+    return responseFormat(OK, result);
+  }
 }
