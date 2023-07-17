d207f8b2ee96f273383a27a36a22d96ffa5dc48f
Z00One <9000248@g.yju.ac.kr>
2023-07-17 23:18:49 +0900

    REMOVE

--- src/decorators/is-not-empty-string.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { applyDecorators } from '@nestjs/common';
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export default function IsNotEmptyString() {
-  return applyDecorators(
-    IsNotEmpty(),
-    IsString()
-  );
-}
