00e23c7f00515d1d1e8ccd4bc9b40a010d7747af
Z00One <9000248@g.yju.ac.kr>
2023-07-17 23:34:09 +0900

    feat: Implement guard for JWT-based authentication

---
+++ src/common/guard/access.guard.ts
@@ -0,0 +1,65 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  GoneException,
+  Res,
+  UnprocessableEntityException
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request, Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AccessGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) { }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnprocessableEntityException();
+    }
+    try {
+      await this.jwtService.verifyAsync(
+        token,
+        {
+          secret: this.configService.get<string>('JWT_ACCESS_SECRET')
+        }
+      );
+      
+      const email = this.jwtService.decode(token);
+      
+      request['body']['acessToken'] = token;
+      request['body']['email'] = email;
+
+    } catch (error) {
+
+      switch (error?.name) {
+        case 'TokenExpiredError': throw new GoneException(error);
+        // 만료된 경우가 아닐 시에 로그인 redirect
+        default: {
+          const response = context.switchToHttp().getResponse();
+          this.reLogin(response);
+        }
+      }
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    try {
+      const [type, token] = request.headers.authorization.split(' ') ?? [];
+      return type === 'Bearer' ? token : undefined;
+    } catch (error) {
+      throw new UnprocessableEntityException(error.name);
+    }
+  }
+
+  async reLogin(@Res() response: Response) {
+    response.redirect(this.configService.get<string>('GOAUTH_RELOGIN_URL'));
+  }
+}
