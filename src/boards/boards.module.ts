9858934d79d325abfd7941e6d9d6fa7ae71d9181
Z00One <9000248@g.yju.ac.kr>
2023-07-26 18:11:17 +0900

    refactor: Add UserModule for authentication

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -4,8 +4,10 @@ import { BoardsController } from './boards.controller';
 import { BoardRepository } from './board.repository';
 import { PrismaService } from 'src/prisma.service';
 import { PostImageRepository } from './boardImage.repository';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [UserModule],
   providers: [
     BoardsService,
     BoardRepository,
