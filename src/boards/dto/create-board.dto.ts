a0ab1b18dc0ffdd35f8c9d7ed901b23137a1e841
hetame1 <poi8946@naver.com>
2023-10-13 19:36:58 +0900

    글 생성 수정

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,7 +1,11 @@
 import { IsNotEmptyString } from 'src/common/decorators/is-not-empty-string.decorator';
 import { IsNotEmptyNumber } from 'src/common/decorators/is-not-empty-number.decorator';
 import { IsOptional } from 'class-validator';
-import { Image } from '@prisma/client';
+
+interface Image {
+  url: string;
+  key: string;
+}
 
 export class CreateBoardDto {
   @IsNotEmptyString()
